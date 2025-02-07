dfeb28bf53bada362a2d92168c6d37b9e6a9491b
Alejandro Lara <alex.lara.cont97@gmail.com>
2025-02-07 13:43:59 +0100

    Plantilla de envío de correo

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/mail")
+def send_mail():
+    return render_template("send_mail.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
