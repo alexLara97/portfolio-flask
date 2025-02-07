d254d669133e8277e171a1a0381bd2670ff50e1a
Alejandro Lara <alex.lara.cont97@gmail.com>
2025-02-07 17:20:26 +0100

    Envío de correo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -6,9 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/mail")
+@app.route("/mail", methods = ['GET', 'POST'])
 def send_mail():
-    return render_template("send_mail.html")
+    if request.method == "POST":
+        # Se obtiene los datos del form
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+        return render_template("send_mail.html")
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
